9118d367c3cc455e4d7c01c1ff32b4fc36619e01
Chad Lew <lewchad11@gmail.com>
2023-07-07 20:03:00 -0700

    Adding dropdown

--- src/components/Dropdown/Dropdown.tsx
+++ src/components/Dropdown/Dropdown.tsx
@@ -1,28 +1,30 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 import {
-    animate,
-    motion,
-    stagger,
-    useAnimate,
-    useAnimationFrame,
-  } from "framer-motion";
-  const staggerMenuItems = stagger(0.1, { startDelay: 0.15 });
+  animate,
+  motion,
+  stagger,
+  useAnimate,
+  useAnimationFrame,
+} from "framer-motion";
+
+import ImageContainer from "../Images/ImageContainer";
+const staggerMenuItems = stagger(0.1, { startDelay: 0.15 });
 const useMenuAnimation = (isOpen: boolean) => {
-    const [scope, animate] = useAnimate();
-    useEffect(() => {
+  const [scope, animate] = useAnimate();
+  useEffect(() => {
     // void animate(".arrow", { rotate: isOpen ? 180 : 0 }, { duration: 0.2 });
 
     void animate(
       "ul",
       {
         clipPath: isOpen
-          ? "inset(0% 0% 0% 0% round 10px)"
-          : "inset(10% 50% 90% 50% round 10px)"
+          ? "inset(0% 0% 0% 0% round 0px)"
+          : "inset(100% 50% 90% 50% round 10px)",
       },
       {
         type: "spring",
         bounce: 0,
-        duration: 0.5
+        duration: 0.5,
       }
     );
 
@@ -33,34 +35,67 @@ const useMenuAnimation = (isOpen: boolean) => {
         : { opacity: 0, scale: 0.3, filter: "blur(20px)" },
       {
         duration: 0.2,
-        delay: isOpen ? staggerMenuItems : 0
+        delay: isOpen ? staggerMenuItems : 0,
       }
     );
-
-
   }, [animate, isOpen]);
 
   return scope;
-}
+};
 
 const Dropdown = () => {
+  const tabs = [
+    "Biodiversity",
+    "River System",
+    "Climate and Environment",
+    "Cultural Diversity",
+    "Threats and Conservation",
+  ];
   const [isOpen, setIsOpen] = useState(false);
+  const [hover, setHover] = useState(false);
+  const [hoverSelectedTab, setHoverSelectedTab] = useState(tabs[0]);
+
   const scope = useMenuAnimation(isOpen);
   const toggleDropdown = () => {
     setIsOpen(!isOpen);
   };
-  
-  return (
-    <div ref={scope}>
-      <button className="m-2 p-2 rounded bg-green-900 font-display font-bold text-yellow-300" onClick={toggleDropdown}>Learn</button>
-        <ul className="w-fit p-2 bg-green-400 rounded font-display font-bold">
-          <li>Biodiversity</li>
-          <li>River System</li>
-          <li>Climate and Environment</li>
-          <li>Cultural Diversity</li>
-          <li>Threats and Conservation</li>
 
-        </ul>
+  return (
+    <div className="flex-col sm:flex-row border-2 w-full flex justify-between sm:items-center" ref={scope}>
+        <div className="inline-block w-1/3">
+      <button
+        className="w-full p-2 rounded-t-md bg-blend-luminosity bg-black font-display font-bold text-yellow-300 shadow-md text-left"
+        onClick={toggleDropdown}
+      >
+        Learn
+      </button>
+      <ul className="bg-slate-00 font-display font-bold relative rounded-b-md shadow-md border-2 border-t-0 border-black">
+        {tabs.map((tab: string, index) => {
+          return (
+            <>
+            <motion.li
+              key={index}
+              className={`${hoverSelectedTab === tab ? "text-white" : "text-slate-900"} relative cursor-pointer hover:text-white transition-colors duration-300 underlineease-in-out p-1`}
+              onHoverStart={() => setHoverSelectedTab(tab)}
+            >
+                  {hoverSelectedTab === tab && (
+                <motion.div
+                layoutId="active-pill"
+                  className="bg-zinc-900 w-full h-8 absolute inset-0 -z-10"
+                //   transition={{ duration: 0.5 }}
+                //   animate={{ x: 100 }}
+                ></motion.div>
+                )}
+              <span className=" z-10">{tab}</span>
+            </motion.li>
+          
+            </>
+          );
+        })}
+      </ul>
+      {/* {hoverSelectedTab} */}
+      </div>
+      <ImageContainer selectedImage={hoverSelectedTab}/>
     </div>
   );
 };
---
+++ src/components/Images/ImageContainer.tsx
@@ -0,0 +1,11 @@
+
+interface ImageContainerProps {
+    selectedImage: string;
+  }
+const ImageContainer = ({selectedImage}: ImageContainerProps) => {
+  return (
+    <div className="w-2/3 border-2">fd</div>
+  )
+}
+
+export default ImageContainer
