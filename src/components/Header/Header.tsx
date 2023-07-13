cec5082767d0db77888d0f099bfd7c0e7a4f0c7b
Chad Lew <lewchad11@gmail.com>
2023-07-12 20:55:13 -0700

    Adding Lottie and fixing dimensions for mobile

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -12,6 +12,8 @@ import { useEffect, useRef } from "react";
 import tree from "../../assets/tree.svg";
 import elephant from "../../assets/elephant.svg";
 import { useMediaQuery } from "usehooks-ts";
+import { Player, Controls } from "@lottiefiles/react-lottie-player";
+import elephantLottie from "../../assets/lottie/animation_lk01znle.json";
 interface HeaderProps {
   content: string;
 }
@@ -22,13 +24,7 @@ const Header = ({ content }: HeaderProps) => {
   const isInView = useInView(ref, { once: true });
   const [scope, animate] = useAnimate();
   useEffect(() => {
-
     // Always snap to the center of the section
-    void animate(
-      ".content",
-      { y: [0, -160], opacity: 1, fontSize: ["6rem", "4rem"] },
-      { type: "spring", duration: 2, delay: 3 }
-    );
 
     if (isInView) {
       void animate(
@@ -46,12 +42,15 @@ const Header = ({ content }: HeaderProps) => {
       void animate(
         ".grassy",
         { y: [1000, 0], opacity: 1 },
-        { type: "spring", duration: 2, delay: 0.2 }
+        { type: "spring", duration: 1.5, delay: 0.2 }
+        
       );
       void animate(
         ".env",
-        { y: [100, 0], opacity: 1 },
-        { type: "spring", duration: 2, delay: 1.5 }
+        matches ?
+        { y: [1100, 0], opacity: 1, scale: [0, 1] }
+        : { y: [1100, -30], opacity: 1, scale: [0, 1] },
+        { type: "spring", duration: 1.2, delay: 1 }
       );
     }
   }, [animate, isInView]);
@@ -68,7 +67,13 @@ const Header = ({ content }: HeaderProps) => {
       >
         {content}
       </h2>
-      <div className="grassy absolute bottom-0 left-0 w-full h-full bg-bottom flex justify-evenly items-end snap-center opacity-0">
+      <div className="grassy absolute bottom-0 left-0 w-full h-full bg-bottom flex justify-center items-end snap-center opacity-0">
+      <Player
+          autoplay
+          loop
+          className="player hidden sm:block w-1/3"
+          src={elephantLottie}
+        ></Player>
         <img
           className="env w-1/2 sm:w-1/3  bottom-[4%] md:bottom-[14%] left-0 opacity-0"
           height="100%"
@@ -76,7 +81,12 @@ const Header = ({ content }: HeaderProps) => {
           src={tree}
           alt=""
         />
-
+     <Player
+      className="player hidden sm:block w-1/3"
+          autoplay
+          loop
+          src={elephantLottie}
+        ></Player>
         <img
           className="env w-1/4 right-0 opacity-0"
           height="100%"
