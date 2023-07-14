5c927c63a3be6be297f291c053db48a39156c275
Chad Lew <lewchad11@gmail.com>
2023-07-14 01:08:30 -0700

    Fixing header aniamtions more

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -23,8 +23,8 @@ const Header = ({ content }: HeaderProps) => {
   const ref = useRef(null);
   const isInView = useInView(ref, { once: true });
   const [scope, animate] = useAnimate();
-  const staggerSpanLetters = stagger(.5, { startDelay: 2 });
-
+  const staggerSpanLettersEven = stagger(.3, { startDelay: 4 });
+  const staggerSpanLettersOdd = stagger(.2, { startDelay: 3 })
   const topics:string[] = [
     'River of Life',
     'Threats',
@@ -33,7 +33,16 @@ const Header = ({ content }: HeaderProps) => {
     'Lungs of the Earth',
     'Climate and Seasons',
     'Spectrum',
+    'Biodiversity',
   ]
+
+  const getRandomPosition = () => {
+    // Get Random Position within the screen
+    const randomX = Math.floor(Math.random() * 100) + 1;
+    const randomY = Math.floor(Math.random() * 100) + 1;
+    return { x: randomX, y: randomY };
+
+  };
 
   const { scrollYProgress } = useScroll();
   useEffect(() => {
@@ -44,13 +53,13 @@ const Header = ({ content }: HeaderProps) => {
       void animate(
         ".content",
         isNotMobile
-          ? {  y2: [0, -160], opacity: 1, fontSize: ["6rem", "4rem"] }
-          : { y: [0, -160], opacity: 1, fontSize: ["2rem", "1.5rem"] },
+          ? {  y: [0, -200], opacity: 1, fontSize: ["6rem", "4rem"] }
+          : { y: [0, -220], opacity: 1, fontSize: ["2rem", "1.5rem"] },
         { type: "spring", duration: 2, delay: 1.5 }
       );
       void animate(
         ".desc",
-        { y: [0, -170], opacity: 1 },
+        { y: [0, -220], opacity: 1 },
         { type: "spring", duration: 2, delay: 2 }
       );
 
@@ -67,22 +76,34 @@ const Header = ({ content }: HeaderProps) => {
         : {  opacity: 1, scale: [0, 0.9], y: -30 },
         { type: "keyframes", duration: 1.2, delay: 3 }
       );
+
+      if (!isNotMobile) {
+      void animate(
+        ".sub-text",
+        {  opacity: 1 },
+        { type: "spring", duration: .4, delay: staggerSpanLettersOdd }
+      );
+      }
+
+      if (isNotMobile) {
       void animate(
         ".sub-text:nth-child(even)",
         isNotMobile ?
-        {  opacity: 1, scale: [0, 1], y: 12, border: "solid 2px black", padding: "10px", background: "#FFF", borderBottomRightRadius: "20px",borderTopLeftRadius: "20px", x: [0, 100] }
+        {  opacity: 1, border: "solid 2px black", padding: "6px", background: "#FFF", borderBottomRightRadius: "20px",borderTopLeftRadius: "20px", x: [0, 100] }
         : {  opacity: 1, scale: [0, 1], border: "solid 2px black", padding: "4px", borderRadius: "4px", y: -100 },
-        { type: "spring", duration: 2, delay: staggerSpanLetters }
+        { type: "spring", duration: .6, delay: staggerSpanLettersEven }
       );
+ 
       void animate(
         ".sub-text:nth-child(odd)",
         isNotMobile ?
-        {  opacity: 1, scale: [0, 1], y: 12, border: "solid 2px black", padding: "10px", background: "#FFF", borderRadius: "4px",borderBottomRightRadius: "20px",borderTopLeftRadius: "20px", x: [0, -100] }
+        {opacity: 1, border: "solid 2px black", padding: "6px", background: "#FFF", borderRadius: "4px",borderBottomRightRadius: "20px",borderTopLeftRadius: "20px", x: [0, -100] }
         : {  opacity: 1, scale: [0, 1], border: "solid 2px black", padding: "4px", borderRadius: "4px", y: -100 },
-        { type: "spring", duration: 2, delay: staggerSpanLetters }
+        { type: "spring", duration: .6, delay: staggerSpanLettersEven }
       );
+      }
     }
-  }, [animate, isInView, isNotMobile, scrollYProgress, staggerSpanLetters]);
+  }, [animate, isInView, isNotMobile, scrollYProgress, staggerSpanLettersEven, staggerSpanLettersOdd]);
 
   return (
     <div
@@ -128,16 +149,16 @@ const Header = ({ content }: HeaderProps) => {
       <p className="desc head-content font-display font-bold text-black text-xl text-center flex justify-center items-center z-10 opacity-0">
         This is the Amazon
       </p>
-      <div className="relative flex justify-center flex-col items-center gap-2">
-      {topics.map((topic, index) => (
+      <div className="absolute inset-16 flex justify-center flex-col items-center h-screen">
+      {topics.map((topic) => (
         <motion.div
           key={topic}
-          initial={{ }} 
-          animate={{ opacity: 1, scale: [0, 0.9] }} // Animate opacity to make the span visible
-          transition={{ delay: index * 0.2 }} 
-          className="display-font sub-text opacity-0 translate-x-3 w-fit cursor-pointer"
+          className={`relative display-font sub-text opacity-0 w-fit cursor-pointer rounded border-2 border-black p-1 mb-2 bg-white z-10`}
         >
           {topic}
+          <div className="absolute top-0 left-0 w-full h-full bg-red-600 opacity-50 -z-10 m-2">
+
+          </div>
         </motion.div>
       ))}
     </div>
