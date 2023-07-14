ba1fdf73ec8c325d21efbdf09d9d95e44bdce80c
Chad Lew <lewchad11@gmail.com>
2023-07-14 12:46:38 -0700

    refactor

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -8,6 +8,7 @@ import {
   useAnimationFrame,
   useInView,
 } from "framer-motion";
+import TopicItem from "../TopicItem/TopicItem";
 import { useEffect, useRef } from "react";
 import tree from "../../assets/tree.svg";
 import elephant from "../../assets/elephant.svg";
@@ -23,83 +24,81 @@ const Header = ({ content }: HeaderProps) => {
   const ref = useRef(null);
   const isInView = useInView(ref, { once: true });
   const [scope, animate] = useAnimate();
-  const staggerSpanLettersEven = stagger(.3, { startDelay: 4 });
-  const staggerSpanLettersOdd = stagger(.2, { startDelay: 3 })
-  const topics:string[] = [
-    'River of Life',
-    'Threats',
-    'Indigenous Cultures',
-    'Conservation Efforts',
-    'Lungs of the Earth',
-    'Climate and Seasons',
-    'Spectrum',
-    'Biodiversity',
-  ]
-
-  const getRandomPosition = () => {
-    // Get Random Position within the screen
-    const randomX = Math.floor(Math.random() * 100) + 1;
-    const randomY = Math.floor(Math.random() * 100) + 1;
-    return { x: randomX, y: randomY };
-
-  };
+  const staggerSpanLettersEven = stagger(0.3, { startDelay: 4 });
+  const staggerSpanLettersOdd = stagger(0.2, { startDelay: 3 });
+  const topics: string[] = [
+    "River of Life",
+    "Threats",
+    "Indigenous Cultures",
+    "Conservation Efforts",
+    "Lungs of the Earth",
+    "Climate and Seasons",
+    "Spectrum",
+    "Biodiversity",
+  ];
 
   const { scrollYProgress } = useScroll();
   useEffect(() => {
     // Check if the element is in view
-    console.log(scrollYProgress)
+    console.log(scrollYProgress);
 
     if (isInView) {
       void animate(
-        ".content",
-        isNotMobile
-          ? {  y: [0, -200], opacity: 1, fontSize: ["6rem", "4rem"] }
-          : { y: [0, -220], opacity: 1, fontSize: ["2rem", "1.5rem"] },
-        { type: "spring", duration: 2, delay: 1.5 }
-      );
-      void animate(
         ".desc",
-        { y: [0, -220], opacity: 1 },
+        { y: [0, -240], opacity: 1 },
         { type: "spring", duration: 2, delay: 2 }
       );
 
       void animate(
         ".grassy",
-        {  opacity: 1 },
+        { opacity: 1 },
         { type: "spring", duration: 1.5, delay: 2.2 }
       );
 
-      void animate(
-        ".env",
-        isNotMobile ?
-        {  opacity: 1, scale: [0, 0.9], y: 12 }
-        : {  opacity: 1, scale: [0, 0.9], y: -30 },
-        { type: "keyframes", duration: 1.2, delay: 3 }
-      );
-
       if (!isNotMobile) {
-      void animate(
-        ".sub-text",
-        {  opacity: 1 },
-        { type: "spring", duration: .4, delay: staggerSpanLettersOdd }
-      );
+        void animate(
+          ".sub-text",
+          { opacity: 1 },
+          { type: "spring", duration: 0.4, delay: staggerSpanLettersOdd }
+        );
+        void animate(
+          ".content",
+          { y: [0, -220], opacity: 1, fontSize: ["2rem", "1.5rem"] },
+          { type: "spring", duration: 2, delay: 1.5 }
+        );
+        void animate(
+          ".env",
+          { opacity: 1, scale: [0, 0.9], y: -30 },
+          { type: "keyframes", duration: 1.2, delay: 3 }
+        );
       }
 
       if (isNotMobile) {
-      void animate(
-        ".sub-text:nth-child(even)",
-        {  opacity: 1, x: [0, 100] }, 
-        { type: "spring", duration: .6, delay: staggerSpanLettersEven }
-      );
- 
-      void animate(
-        ".sub-text:nth-child(odd)",
-        {opacity: 1, x: [0, -100] },
-        { type: "spring", duration: .6, delay: staggerSpanLettersEven }
-      );
+        void animate(
+          ".sub-text",
+          { opacity: 1 },
+          { type: "spring", duration: 0.6, delay: staggerSpanLettersEven }
+        );
+        void animate(
+          ".content",
+          { y: [0, -250], opacity: 1, fontSize: ["6rem", "4rem"] },
+          { type: "spring", duration: 2, delay: 1.5 }
+        );
+        void animate(
+          ".env",
+          { opacity: 1, scale: [0, 0.9], y: 12 },
+          { type: "keyframes", duration: 1.2, delay: 3 }
+        );
       }
     }
-  }, [animate, isInView, isNotMobile, scrollYProgress, staggerSpanLettersEven, staggerSpanLettersOdd]);
+  }, [
+    animate,
+    isInView,
+    isNotMobile,
+    scrollYProgress,
+    staggerSpanLettersEven,
+    staggerSpanLettersOdd,
+  ]);
 
   return (
     <div
@@ -114,7 +113,7 @@ const Header = ({ content }: HeaderProps) => {
         {content}
       </h2>
       <div className="grassy absolute bottom-0 left-0 w-full h-full bg-bottom flex justify-center items-end snap-center opacity-0">
-      <Player
+        <Player
           autoplay
           loop
           className="player hidden sm:block w-1/3"
@@ -126,41 +125,42 @@ const Header = ({ content }: HeaderProps) => {
           src={tree}
           alt=""
         />
-            <img
+        <img
           className="env w-1/4 opacity-0"
           height="100%"
           src={elephant}
           alt=""
-          style={{transform: "translateY(50px) translateX(50px)"}}
+          style={{ transform: "translateY(50px) translateX(50px)" }}
         />
-     <Player
-      className="player hidden sm:block w-1/3"
+        <Player
+          className="player hidden sm:block w-1/3"
           autoplay
           loop
           src={elephantLottie}
         ></Player>
-    
       </div>
 
       <p className="desc head-content font-display font-bold text-black text-xl text-center flex justify-center items-center z-10 opacity-0">
         This is the Amazon
       </p>
       <div className="absolute inset-16 flex justify-center flex-col items-center h-screen z-10">
-      {topics.map((topic) => (
-     <motion.div
-     key={topic}
-
-     className={`relative display-font sub-text opacity-0 w-fit cursor-pointer p-1 mb-2 z-50`}
-   >
-     <div className="absolute top-2 left-2 w-full h-full bg-slate-600 -z-10 rounded border-2 border-black rounded-br-xl"></div>
-     <motion.div
-          whileTap={{ x: 5, y: 5 }}
-     className="inner-div relative z-0 p-2 rounded border-2 border-black rounded-br-xl" style={{ backgroundColor: "#06B6D4" }}>
-       {topic}
-     </motion.div>
-   </motion.div>
-      ))}
-    </div>
+        {topics.map((topic) => (
+          <motion.div
+            key={topic}
+            className={`relative display-font sub-text opacity-0 w-fit cursor-pointer p-1 mb-2 z-50`}
+          >
+            <div className="absolute top-2 left-2 w-full h-full bg-slate-600 -z-10 rounded border-2 border-black rounded-br-xl blur-sm"></div>
+            <motion.div
+              whileTap={{ x: 5, y: 5 }}
+              className="inner-div relative z-0 p-2 rounded border-2 border-black rounded-br-xl text-slate-100 "
+              style={{ backgroundColor: "#635799" }}
+            >
+              {topic}
+            </motion.div>
+          </motion.div>
+        ))}
+        {/* <TopicItem/> */}
+      </div>
     </div>
   );
 };
---
+++ src/components/TopicItem/TopicItem.tsx
@@ -0,0 +1,9 @@
+
+
+const TopicItem = () => {
+  return (
+    <div className=" absolute border-4 border-black h-64 w-64 bg-red-500">TopicItem</div>
+  )
+}
+
+export default TopicItem
