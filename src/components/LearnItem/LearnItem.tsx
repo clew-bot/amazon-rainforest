353ea47342cce81b61986b02431b245db77332ee
Chad Lew <lewchad11@gmail.com>
2023-07-28 14:07:23 -0700

    Fixing transition from learnitem and section

--- src/components/LearnItem/LearnItem.tsx
+++ src/components/LearnItem/LearnItem.tsx
@@ -1,36 +1,63 @@
-import { AnimatePresence, motion, animate, stagger, useAnimate } from "framer-motion";
-import { useEffect, useState } from "react";
+import { AnimatePresence, motion, animate, stagger, useAnimate, useInView } from "framer-motion";
+import { useEffect, useState, useRef } from "react";
 import learn from "../../json/learn.json";
 interface LearnItemProps {
   selectedTab: string;
 }
 
+
+const staggerLearnItems = stagger(0.1, { startDelay: 0.15 });
+
+
 const LearnItem = ({ selectedTab }: LearnItemProps) => {
-    const [scope, animate] = useAnimate();
-    const staggerLearnItems = stagger(0.1, { startDelay: 0.15 });
-  useEffect(() => {
-    void animate(
-        ".learnItem",
-        {  opacity: 1, scale: [0.,1] },
-        { type: "spring", duration: .3, delay: staggerLearnItems }
-      );
-  }, [selectedTab]);
+    const ref = useRef(null);
+    const isInView = useInView(ref, { once: true });
+
+    const useLearnAnimation = () => {
+   
+        const [scope, animate] = useAnimate();
+        useEffect(() => {
+            if (isInView) {
+                console.log("run")
+            void animate(
+                ".learnItem",
+                {  opacity: 1, scale: [0, 0.9] },
+                { type: "spring", duration: .3, delay: staggerLearnItems }
+              );
+              void animate(
+                ".learn-container",
+                {  opacity: 1, height: "40rem" },
+                { type: "spring", duration: .3, delay: staggerLearnItems }
+              );
+            }
+          }, [animate, isInView, selectedTab]);
+
+          return scope;
+    }
+
+    const scope = useLearnAnimation();
+
 
   return (
     <div
+    
     ref={scope}
-    className="right-0 absolute w-1/2 border-4 border-black rounded-md h-full font-display">
-      <div className="bg-white">
+    className="right-0 absolute w-1/2 border-4 border-black rounded-md font-display">
+      <div 
+         ref={ref}
+      className="bg-black">
         <h2 className="w-full p-2 rounded-t-md bg-blend-luminosity bg-black font-display font-bold text-slate-100 shadow-md text-left text-4xl underline ">
           Discover
         </h2>
-        <div className="">
+        <div className="bg-white">
           {learn.map(
             (item) =>
               item &&
               item.title === selectedTab && (
-                <>
-                  <div className="learnItem text-center text-xl font-bold pt-2">
+                <div key={item.title} className="learn-container h-60">
+                  <div
+                 
+                  className="learnItem text-center text-xl font-bold pt-2">
                     <h3>{item.title}</h3>
                   </div>
                   <div className="learnItem p-4 opacity-0">
@@ -42,7 +69,7 @@ const LearnItem = ({ selectedTab }: LearnItemProps) => {
                   <div className="learnItem text-left">
                     <p>{item.sections[0].funFact}</p>
                   </div>
-                </>
+                </div>
               )
           )}
         </div>
